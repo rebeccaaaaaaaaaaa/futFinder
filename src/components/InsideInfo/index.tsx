6d5102bd370181cfb974ca6e22764a8fb4d0df1c
rebeccaaaaaaaaaaa <rebecca.ignacio@orramtech.com.br>
2022-05-04 15:47:07 -0300

    criando a página de detalhes

---
+++ src/components/InsideInfo/index.tsx
@@ -0,0 +1,7 @@
+export function InsideInfo() {
+  return (
+    <div>
+        <h1> Detalhes </h1>
+    </div>
+  )
+}
--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -1,11 +1,14 @@
 import { BackButton } from "../components/BackButton";
 import { Header } from "../components/Header";
+import { InsideInfo } from "../components/InsideInfo";
 
 export function Details() {
   return (
     <div>
       <Header />
       <BackButton />
+
+      <InsideInfo />
     </div>
   )
 }
